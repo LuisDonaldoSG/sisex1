0336b952797f77f0ab8d7370e5c44730b04c9a27
Donaldo <51167382+LuisDonaldoSG@users.noreply.github.com>
2021-11-26 01:15:35 -0600

    ya alv

---
+++ script.js
@@ -0,0 +1,296 @@
+let arr = [
+    {puntos : [
+        {materia : 'Sistemas operativos', num : 0},
+        {materia : 'Programación de sistemas', num : 0},
+        {materia : 'Arquitectura de sistemas de red', num : 0},
+        {materia : 'Sistemas operativos aplicados', num : 0},
+        {materia : 'Mantenimiento de equipo de computo', num : 0},
+        {materia : 'Habilidades de comunicación', num : 0},
+        {materia : 'Administración de proyectos de red', num : 0},
+        {materia : 'Prácticas de redes', num : 0},
+        {materia : 'Metodología de la investigación', num : 0},
+        {materia : 'Estructura de datos', num : 0},
+        {materia : 'Principios de telecomunicaciones', num : 0},
+        {materia : 'Seguridad', num : 0},
+        {materia : 'Administración de servidores', num : 0},
+        {materia : 'Administración avanzada de servicios', num : 0},
+        {materia : 'Análisis de protocolos', num : 0},
+        {materia : 'Enrutamiento básico', num : 0},
+        {materia : 'Enrutamiento avanzado', num : 0},
+        {materia : 'Redes inalámbricas', num : 0},
+        {materia : 'Redes WAN', num : 0},
+        {materia : 'Administración de proyectos de red', num : 0},
+        {materia : 'Programación segura', num : 0},
+        {materia : 'Arquitectura en red', num : 0},
+        {materia : 'Programación en la administración de redes', num : 0},
+        {materia : 'Lectura y redacción a través del análisis del mundo contemporáneo', num : 0},
+        {materia : 'Probabilidad y estadística para computación', num : 0},
+        {materia : 'Persona y proyecto de vida', num : 0},
+        {materia : 'Ética y profesión', num : 0},
+        {materia : 'Diseño y desarrollo de videojuegos', num : 0},
+        {materia : 'Diseño y proyectos de investigación', num : 0},
+        {materia : 'Sensores y actuadores', num : 0},
+        {materia : 'Diseño de compiladores', num : 0},
+        {materia : 'Gráficas por computadora', num : 0},
+        {materia : 'Ingeniería de software', num : 0},
+        {materia : 'Ingeniería de software II', num : 0},
+    ]},
+    {
+        ques: "¿Has tomado algún curso dentro o fuera del centro educativo que acredite un nivel igual o superior a B2?",
+        ans: null,
+        cat: "tronco común",
+        asignatures: [
+            'Inglés I', 'Inglés II'
+        ]
+    },
+    {
+        ques: "¿Acreditas un nivel medio o avanzado de algún lenguaje de programación?",
+        ans: null,
+        cat: "tronco común",
+        asignatures: [
+            'Introducción a la programación'
+        ]
+    },
+    {
+        ques: "¿Estás familiarizado con funcionamiento interno de un servidor?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Servicios y servidores'
+        ]
+    },
+    {
+        ques: "¿Tienes una noción de lo que es un protocolo de red?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Servicios y servidores'
+        ]
+    },
+    {
+        ques: "¿Estás interesado en conocer el funcionamiento avanzado de un servidor?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Servicios y servidores'
+        ]
+    },
+    {
+        ques: "¿Sabes para que sirven los servicios que ofrece un servidor?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Servicios y servidores'
+        ]
+    },
+    {
+        ques: "¿Te interesa poder administrar los recursos de una plataforma de servicios?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Servicios y servidores'
+        ]
+    },
+    {
+        ques: "¿Estás interesado en cómo puede ser ensamblado desde cero un servidor?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Arquitectura de red'
+        ]
+    },
+    {
+        ques: "¿La creación de redes físicas, así como su configuración son de tu interés?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Arquitectura de red'
+        ]
+    },
+    {
+        ques: "Los servidores previo a su configuración interna requieren un ensamble previo, ¿te interesa conocer los pasos para ensamblarlo?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Arquitectura de red'
+        ]
+    },
+    {
+        ques: "¿Quieres conocer cómo funcionan las señales que proporcionan internet a tu teléfono?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Arquitectura de red'
+        ]
+    },
+    {
+        ques: "¿Te interesa crear y configurar redes a gran escala?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Arquitectura de red'
+        ]
+    },
+    {
+        ques: "¿Eres un fanático de lo que los hackers de sombrero blanco pueden hacer?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Seguridad en redes'
+        ]
+    },
+    {
+        ques: "¿Te resulta interesante la protección de datos dentro de un servidor?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Seguridad en redes'
+        ]
+    },
+    {
+        ques: "¿Quieres conocer cómo protegerte antes amenazas digitales?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Seguridad en redes'
+        ]
+    },
+    {
+        ques: "¿Te interesa la programación de servicios desde una perspectiva de seguridad?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Seguridad en redes'
+        ]
+    },
+    {
+        ques: "¿La aplicación de protocolos de seguridad te resulta familiar en algún sentido?",
+        ans: null,
+        cat: "especialidad",
+        asignatures: [
+            'Seguridad en redes'
+        ]
+    },
+    {
+        ques: "¿Te ves como un futuro desarrollador de proyectos de sistemas dedicados?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Ingeniería de software I',
+            'Ingeniería de software II',
+            'Diseño de proyectos'
+        ]
+    },
+    {
+        ques: "¿Eres un apasionado de los videojuegos o te interesa desarrollar alguno?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Ingeniería de Graficas por computadora', 'Diseño y desarrollo de videojuegos'
+        ]
+    },
+    {
+        ques: "¿Te consideras una persona en busca de retos matemáticos o de problemas complejos?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Algebra lineal para computación', 'Probabilidad y estadística para computación'
+        ]
+    },
+    {
+        ques: "¿Quieres explorar tu lado humanista durante tu desarrollo profesional?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Persona y proyecto de vida', 'Ética y profesión', 'Lectura y redacción a través den análisis del mundo contemporáneo'
+        ]
+    },
+    {
+        ques: "¿Te interesa la idea de aprender cómo funcionan los dispositivos eléctricos y como programarlos? ",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Diseño de compiladores', 'Sensores y actuadores'
+        ]
+    },
+    {
+        ques: "¿Eres una persona que se caractericé por buscar resultados de forma exacta?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Algebra lineal para computación', 'Probabilidad y estadística para computación'
+        ]
+    },
+    {
+        ques: "¿Algunas vez has pensado en crear diseños tridimensionales?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Gráficas por computadora', 'Diseño de videojuegos'
+        ]
+    },
+    {
+        ques: "¿Consideras que es importante crecer como persona en tu día a día?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Persona y proyecto de vida', 'Ética y profesión', 'Lectura y redacción a través del análisis del mundo contemporáneo'
+        ]
+    },
+    {
+        ques: "¿Piensas que los sistemas computacionales son las herramientas del mañana?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Ingeniera de software I', 'Ingeniera de software II'
+        ]
+    },
+    {
+        ques: "¿Te interesa programar en una gran área de sistemas?",
+        ans: null,
+        cat: "optativa",
+        asignatures: [
+            'Diseño de compiladores', 'Diseño y proyectos de investigación', 'Sensores y actuadores'
+        ]
+    }
+]
+
+
+const cambiar = (asn, pos) => {
+    console.log(asn, pos)
+    arr[pos].ans = asn
+
+}
+
+const enviar = () =>{
+    for (let i = 1; i < arr.length; i++){
+        let respuesta = $("#pregunta"+i).val()
+        arr[i].ans = respuesta
+
+        if (arr[i].ans === 'true' ){
+            console.log(arr[i].ans)
+            for (let j = 0; j < arr[0].puntos.length; j++) {
+                let materia = arr[0].puntos[j].materia
+                for (let k = 0; k < arr[i].asignatures.length ; k++){
+                    if (materia === arr[i].asignatures[k]){
+                        arr[0].puntos[j].num = arr[0].puntos[j].num + 1
+                    }
+                }
+            }
+        }
+
+    }
+    console.log(arr)
+}
+
+$(document).ready(function() {
+
+    for (let i = 1; i < arr.length; i++) {
+        console.log(arr[i].ques)
+        $(".preguntas").append(
+            '<li class="list-group-item">'+arr[i].ques+
+            '<select id = "pregunta'+i+'" name="pregunta'+i+'" class ="float-end"> <option value="true">si</option> <option value="false" selected>no</option>  </select>'+
+            '</li>'
+        )
+    }
+})
